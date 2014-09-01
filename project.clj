538f88b9d07aacaa5e429ca4d99673294eb5094b
Laurens Van Houtven <_@lvh.io>
2014-09-01 15:42:02 +0200

    Wait, snapshots are *before* the release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject caesium "0.1.1-SNAPSHOT"
+(defproject caesium "0.1.2-SNAPSHOT"
   :description "libsodium for clojure"
   :url "https://github.com/lvh/caesium"
   :license {:name "Eclipse Public License"
