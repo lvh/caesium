ee83ef6f6342da5fcf05d3b578badf9f3be28f78
Laurens Van Houtven <_@lvh.io>
2014-09-10 11:58:44 +0200

    Bump kalium version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.abstractj.kalium/kalium "0.2.1" :scope "compile"]]
+                 [org.abstractj.kalium/kalium "0.3.0" :scope "compile"]]
   :main ^:skip-aot caesium.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
