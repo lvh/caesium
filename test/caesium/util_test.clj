98a4e1fbe2523538d5db4994b3c89587f9f63979
Laurens Van Houtven <_@lvh.io>
2016-04-02 12:31:54 -0500

    Explicit refer

--- test/caesium/util_test.clj
+++ test/caesium/util_test.clj
@@ -1,20 +1,21 @@
 (ns caesium.util-test
-  (:require [caesium.util :refer :all]
-            [clojure.test :refer :all]))
+  (:require
+   [caesium.util :as u]
+   [clojure.test :refer :all]))
 
 (deftest array-eq-test
   (testing "array equality works"
-    (are [a] (array-eq a a)
+    (are [a] (u/array-eq a a)
       (byte-array [])
       (byte-array [90])))
   (testing "array inequality works"
-    (are [a b] (not (array-eq a b))
+    (are [a b] (not (u/array-eq a b))
       (byte-array []) (byte-array [90])
       (byte-array [90]) (byte-array []))))
 
 (deftest unhexify-test
   (testing "unhexify works"
-    (are [hex raw] (= (vec (unhexify hex))
+    (are [hex raw] (= (vec (u/unhexify hex))
                       (vec (byte-array raw)))
       "" []
       "01" [1]
@@ -24,8 +25,7 @@
 
 (deftest hexify-text
   (testing "hexify works"
-    (are [hex raw] (= (hexify (byte-array raw))
-                      hex)
+    (are [hex raw] (= hex (u/hexify (byte-array raw)))
       "" []
       "01" [1]
       "02" [2]
