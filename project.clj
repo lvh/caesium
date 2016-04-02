c49d3a19c0a8a1578c6bd6b7834086b44fe5c9d2
Laurens Van Houtven <_@lvh.io>
2016-04-02 17:43:01 -0500

    Version 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject caesium "0.4.0-SNAPSHOT"
+(defproject caesium "0.4.0"
   :description "libsodium for clojure"
   :url "https://github.com/lvh/caesium"
   :license {:name "Eclipse Public License"
