3e6e742b4cf2485decee20ed956fc1c5a09f1a34
Laurens Van Houtven <_@lvh.io>
2016-04-16 12:37:50 -0700

    Make interface data available as values

--- src/caesium/binding.clj
+++ src/caesium/binding.clj
@@ -4,47 +4,89 @@
            [jnr.ffi.annotations In Out Pinned LongLong]
            [jnr.ffi.types size_t]))
 
-(definterface Sodium
-  (^int sodium_init [])
-  (^String sodium_version_string [])
+(def ^:private bound-fns
+  "A mapping of type- and jnr.ffi-annotated bound method symbols to
+  respective argspec.
 
-  (^void randombytes
-   [^bytes ^{Pinned {}} buf
-    ^long ^{LongLong {}} buflen])
+  This exists so that tooling (like magic macro helpers) can easily
+  inspect caesium allegedly binds. That can be done by reflecting on
+  the interface too, but that's significantly less convenient;
+  Clojure's reflection tools don't show annotations, and we always use
+  the data in metadata-annotated form anyway (both to create the
+  interface and to bind fns to vars)."
+  '{^int sodium_init []
+    ^String sodium_version_string []
 
-  (^long ^{size_t {}} crypto_secretbox_keybytes [])
-  (^long ^{size_t {}} crypto_secretbox_noncebytes [])
-  (^long ^{size_t {}} crypto_secretbox_macbytes [])
-  (^String ^{size_t {}} crypto_secretbox_primitive[])
+    ^void randombytes
+    [^bytes ^{Pinned {}} buf
+     ^long ^{LongLong {}} buflen]
 
-  (^long ^{size_t {}} crypto_generichash_bytes_min [])
-  (^long ^{size_t {}} crypto_generichash_bytes_max [])
-  (^long ^{size_t {}} crypto_generichash_bytes [])
-  (^long ^{size_t {}} crypto_generichash_keybytes_min [])
-  (^long ^{size_t {}} crypto_generichash_keybytes_max [])
-  (^long ^{size_t {}} crypto_generichash_keybytes [])
-  (^String crypto_generichash_primitive [])
-  (^int crypto_generichash
-   [^bytes ^{Pinned {}} buf
-    ^long ^{LongLong {}} buflen
-    ^bytes ^{Pinned {}} msg
-    ^long ^{LongLong {}} msglen
-    ^bytes ^{Pinned {}} key
-    ^long ^{LongLong {}} keylen])
+    ^long ^{size_t {}} crypto_secretbox_keybytes []
+    ^long ^{size_t {}} crypto_secretbox_noncebytes []
+    ^long ^{size_t {}} crypto_secretbox_macbytes []
+    ^String ^{size_t {}} crypto_secretbox_primitive[]
 
-  ;; TODO: how do I reference a crypto_generichash_state *?
+    ^int crypto_secretbox_easy
+    [^bytes ^{Pinned {}} c
+     ^bytes ^{Pinned {}} m
+     ^long ^{LongLong {}} mlen
+     ^bytes ^{Pinned {}} n
+     ^bytes ^{Pinned {}} k]
+    ^int crypto_secretbox_open_easy
+    [^bytes ^{Pinned {}} m
+     ^bytes ^{Pinned {}} c
+     ^long ^{LongLong {}} clen
+     ^bytes ^{Pinned {}} n
+     ^bytes ^{Pinned {}} k]
+    ^int crypto_secretbox_detached
+    [^bytes ^{Pinned {}} c
+     ^bytes ^{Pinned {}} mac
+     ^bytes ^{Pinned {}} m
+     ^long ^{LongLong {}} mlen
+     ^bytes ^{Pinned {}} n
+     ^bytes ^{Pinned {}} k]
+    ^int crypto_secretbox_open_detached
+    [^bytes ^{Pinned {}} m
+     ^bytes ^{Pinned {}} c
+     ^bytes ^{Pinned {}} mac
+     ^long ^{LongLong {}} clen
+     ^bytes ^{Pinned {}} n
+     ^bytes ^{Pinned {}} k]
 
-  (^int crypto_hash_sha256_bytes [])
-  (^int crypto_hash_sha256
-   [^bytes ^{Pinned {}} buf
-    ^bytes ^{Pinned {}} msg
-    ^long ^{LongLong {}} msglen])
+    ^long ^{size_t {}} crypto_generichash_bytes_min []
+    ^long ^{size_t {}} crypto_generichash_bytes_max []
+    ^long ^{size_t {}} crypto_generichash_bytes []
+    ^long ^{size_t {}} crypto_generichash_keybytes_min []
+    ^long ^{size_t {}} crypto_generichash_keybytes_max []
+    ^long ^{size_t {}} crypto_generichash_keybytes []
+    ^String crypto_generichash_primitive []
+    ^int crypto_generichash
+    [^bytes ^{Pinned {}} buf
+     ^long ^{LongLong {}} buflen
+     ^bytes ^{Pinned {}} msg
+     ^long ^{LongLong {}} msglen
+     ^bytes ^{Pinned {}} key
+     ^long ^{LongLong {}} keylen]
 
-  (^int crypto_hash_sha512_bytes [])
-  (^int crypto_hash_sha512
-   [^bytes ^{Pinned {}} buf
-    ^bytes ^{Pinned {}} msg
-    ^long ^{LongLong {}} msglen]))
+    ;; TODO: how do I reference a crypto_generichash_state *?
+
+    ^int crypto_hash_sha256_bytes []
+    ^int crypto_hash_sha256
+    [^bytes ^{Pinned {}} buf
+     ^bytes ^{Pinned {}} msg
+     ^long ^{LongLong {}} msglen]
+
+    ^int crypto_hash_sha512_bytes []
+    ^int crypto_hash_sha512
+    [^bytes ^{Pinned {}} buf
+     ^bytes ^{Pinned {}} msg
+     ^long ^{LongLong {}} msglen]})
+
+(defmacro ^:private defsodium
+  []
+  `(definterface ~'Sodium ~@(seq bound-fns)))
+
+(defsodium)
 
 (def ^Sodium sodium
   "The sodium library singleton instance."
