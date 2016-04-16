add4aa54ce9b538cddba1cccf1065832ae8a23a9
Laurens Van Houtven <_@lvh.io>
2016-04-16 14:14:05 -0700

    Bind generichash

--- src/caesium/crypto/generichash.clj
+++ src/caesium/crypto/generichash.clj
@@ -1,5 +1,6 @@
 (ns caesium.crypto.generichash
   (:refer-clojure :exclude [bytes])
-  (:require [caesium.binding :refer [sodium defconsts]]))
+  (:require [caesium.binding :refer [sodium defconsts defbindings]]))
 
 (defconsts [bytes bytes-min bytes-max keybytes keybytes-min keybytes-max])
+(defbindings [generichash])
