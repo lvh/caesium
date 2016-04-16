d51c20ad0f96ddf43db1297c40c957db5b94fe0f
Laurens Van Houtven <_@lvh.io>
2016-04-16 14:13:23 -0700

    Use macros for hash ns

--- src/caesium/crypto/hash.clj
+++ src/caesium/crypto/hash.clj
@@ -1,22 +1,5 @@
 (ns caesium.crypto.hash
-  (:require [caesium.binding :refer [sodium]]))
+  (:require [caesium.binding :refer [sodium defconsts defbindings]]))
 
-(def ^:private sha256-bytes
-  (.crypto_hash_sha256_bytes sodium))
-
-(defn sha256
-  "Computes the SHA-256 digest of the given message with `crypto_hash_sha256`."
-  [^bytes msg]
-  (let [buf (byte-array sha256-bytes)]
-    (.crypto_hash_sha256 sodium buf msg (alength msg))
-    buf))
-
-(def ^:private sha512-bytes
-  (.crypto_hash_sha512_bytes sodium))
-
-(defn sha512
-  "Computes the SHA-512 digest of the given message with `crypto_hash_sha512`."
-  [^bytes msg]
-  (let [buf (byte-array sha512-bytes)]
-    (.crypto_hash_sha512 sodium buf msg (alength msg))
-    buf))
+(defconsts [sha256-bytes sha512-bytes])
+(defbindings [sha256 sha512])
