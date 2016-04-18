df8dae84b97b760077be918b188076687e9b9e25
Laurens Van Houtven <_@lvh.io>
2016-04-18 16:54:14 -0500

    Split up SHA-256,512 tests

--- test/caesium/crypto/hash_test.clj
+++ test/caesium/crypto/hash_test.clj
@@ -13,23 +13,25 @@
 
 (def empty-string (byte-array 0))
 
+(def ^:private sha256-vector
+  (comp hash-vector (partial str "sha256/")))
+
 (def sha256-message
   (.getBytes "My Bonnie lies over the ocean, my Bonnie lies over the sea"))
 
-(def sha512-message
-  (.getBytes "My Bonnie lies over the ocean, Oh bring back my Bonnie to me"))
-
-(def ^:private sha256-vector
-  (comp hash-vector (partial str "sha256/")))
+(deftest sha-256-test
+  (are [f message expected] (array-eq expected (f message))
+    h/sha256 sha256-message (sha256-vector "digest-0")
+    h/sha256 empty-string (sha256-vector "digest-empty-string")))
 
 (def ^:private sha512-vector
   (comp hash-vector (partial str "sha512/")))
 
-(deftest sha-256-512-test
-  (are [f message expected] (array-eq expected (f message))
-    h/sha256 sha256-message (sha256-vector "digest-0")
-    h/sha256 empty-string (sha256-vector "digest-empty-string")
+(def sha512-message
+  (.getBytes "My Bonnie lies over the ocean, Oh bring back my Bonnie to me"))
 
+(deftest sha-512-test
+  (are [f message expected] (array-eq expected (f message))
     h/sha512 sha512-message (sha512-vector "digest-0")
     h/sha512 empty-string (sha512-vector "digest-empty-string")))
 
