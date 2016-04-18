c64af95682877e235d91ea2432781c8ce267d7d9
Laurens Van Houtven <_@lvh.io>
2016-04-18 16:34:18 -0500

    Move init from core to sodium

--- src/caesium/core.clj
+++ src/caesium/core.clj
@@ -1,15 +1,1 @@
-(ns caesium.core
-  (:require [caesium.binding :refer [sodium]]))
-
-(defn init
-  "Initializes libsodium.
-
-  This should be called once, when your application starts. It is
-  idempotent: calling it doesn't do anything if it had already been
-  called. It is *not* thread-safe (until it has completed once): if
-  you call it in thread A, you can't concurrently call it in thread B,
-  until it finishes executing in thread A. Usually this is not a
-  problem: just run it synchronously in your application's init
-  routine."
-  []
-  (.sodium_init sodium))
+(ns caesium.core)
