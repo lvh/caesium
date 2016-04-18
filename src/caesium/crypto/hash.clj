1589688f1c3e39e435d5ddd1855710374bf21fbc
Laurens Van Houtven <_@lvh.io>
2016-04-18 16:52:52 -0500

    Don't use defbindings for hash impls

--- src/caesium/crypto/hash.clj
+++ src/caesium/crypto/hash.clj
@@ -1,5 +1,24 @@
 (ns caesium.crypto.hash
-  (:require [caesium.binding :refer [sodium defconsts defbindings]]))
+  (:require [caesium.binding :refer [sodium defconsts]]))
 
 (defconsts [sha256-bytes sha512-bytes])
-(defbindings [sha256 sha512])
+
+(defn sha256-to-buf!
+  [buf msg]
+  (.crypto_hash_sha256 sodium buf msg (alength ^bytes msg)))
+
+(defn sha256
+  [msg]
+  (let [buf (byte-array sha256-bytes)]
+    (sha256-to-buf! buf msg)
+    buf))
+
+(defn sha512-to-buf!
+  [buf msg]
+  (.crypto_hash_sha512 sodium buf msg (alength ^bytes msg)))
+
+(defn sha512
+  [msg]
+  (let [buf (byte-array sha512-bytes)]
+    (sha512-to-buf! buf msg)
+    buf))
