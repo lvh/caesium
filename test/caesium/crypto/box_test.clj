a8589ed1ae9dc218263879e3bbad4b87d8ddfd92
Laurens Van Houtven <_@lvh.io>
2016-04-20 09:52:28 -0500

    Explicit namespaces

--- test/caesium/crypto/box_test.clj
+++ test/caesium/crypto/box_test.clj
@@ -1,20 +1,20 @@
 (ns caesium.crypto.box-test
   (:require
-   [caesium.crypto.box :refer :all]
-   [caesium.util :refer :all]
+   [caesium.crypto.box :as b]
+   [caesium.util :as u]
    [clojure.test :refer :all]
    [caesium.vectors :as v]))
 
 (deftest box-keypair-generation
   (testing "generates new keypairs"
-    (is (let [kp1 (generate-keypair)
-              kp2 (generate-keypair)]
-          (and (not (array-eq (:public kp1) (:public kp2)))
-               (not (array-eq (:secret kp1) (:secret kp2)))))))
+    (is (let [kp1 (b/generate-keypair)
+              kp2 (b/generate-keypair)]
+          (and (not (u/array-eq (:public kp1) (:public kp2)))
+               (not (u/array-eq (:secret kp1) (:secret kp2)))))))
   (testing "generate public key from secret key"
-    (is (let [kp1 (generate-keypair)
-              kp2 (generate-keypair (:secret kp1))]
-          (array-eq (:public kp1) (:public kp2))))))
+    (is (let [kp1 (b/generate-keypair)
+              kp2 (b/generate-keypair (:secret kp1))]
+          (u/array-eq (:public kp1) (:public kp2))))))
 
 (def nonce (v/hex-resource "vectors/box/nonce"))
 (def plaintext (v/hex-resource "vectors/box/plaintext"))
@@ -25,14 +25,14 @@
 (def alice-public-key (v/hex-resource "vectors/box/alice-public-key"))
 
 (deftest box-encrypt-decrypt-test
-  (is (array-eq ciphertext
-                (encrypt alice-public-key bob-secret-key nonce plaintext))
+  (is (u/array-eq ciphertext
+                  (b/encrypt alice-public-key bob-secret-key nonce plaintext))
       "Bob can encrypt a message for Alice")
-  (is (array-eq plaintext
-                (decrypt bob-public-key alice-secret-key nonce ciphertext))
+  (is (u/array-eq plaintext
+                  (b/decrypt bob-public-key alice-secret-key nonce ciphertext))
       "Alice can decrypt the message from Bob")
-  (let [hex-public-key (hexify alice-public-key)
-        hex-private-key (hexify bob-secret-key)]
+  (let [hex-public-key (u/hexify alice-public-key)
+        hex-private-key (u/hexify bob-secret-key)]
     (is (thrown? java.lang.ClassCastException
-                 (encrypt hex-public-key hex-private-key nonce plaintext))
+                 (b/encrypt hex-public-key hex-private-key nonce plaintext))
         "encrypt does not accept strings")))
