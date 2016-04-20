f08f721ac0c24a5b6373daaf6185ff33c9b4547f
Laurens Van Houtven <_@lvh.io>
2016-04-20 10:01:15 -0500

    Clean up tests a little

--- test/caesium/crypto/box_test.clj
+++ test/caesium/crypto/box_test.clj
@@ -25,23 +25,19 @@
               kp2 (b/generate-keypair (:secret kp1))]
           (u/array-eq (:public kp1) (:public kp2))))))
 
-(def nonce (v/hex-resource "vectors/box/nonce"))
-(def plaintext (v/hex-resource "vectors/box/plaintext"))
-(def ciphertext (v/hex-resource "vectors/box/ciphertext"))
-(def bob-secret-key (v/hex-resource "vectors/box/bob-secret-key"))
-(def bob-public-key (v/hex-resource "vectors/box/bob-public-key"))
-(def alice-secret-key (v/hex-resource "vectors/box/alice-secret-key"))
-(def alice-public-key (v/hex-resource "vectors/box/alice-public-key"))
+(def box-vector (partial v/hex-resource "vectors/box/"))
 
 (deftest box-encrypt-decrypt-test
-  (is (u/array-eq ciphertext
-                  (b/encrypt alice-public-key bob-secret-key nonce plaintext))
-      "Bob can encrypt a message for Alice")
-  (is (u/array-eq plaintext
-                  (b/decrypt bob-public-key alice-secret-key nonce ciphertext))
-      "Alice can decrypt the message from Bob")
-  (let [hex-public-key (u/hexify alice-public-key)
-        hex-private-key (u/hexify bob-secret-key)]
+  (let [nonce (box-vector "nonce")
+        ptext (box-vector "plaintext")
+        ctext (box-vector "ciphertext")
+        bob-pk (box-vector "bob-public-key")
+        bob-sk (box-vector "bob-sk")
+        alice-pk (box-vector "alice-public-key")
+        alice-sk (box-vector "alice-secret-key")]
+    (is (u/array-eq ctext (b/encrypt alice-pk bob-sk nonce ptext)))
+    (is (u/array-eq ptext (b/decrypt bob-pk alice-sk nonce ctext))))
+  (let [hex-pk (u/hexify (box-vector "alice-pk"))
+        hex-sk (u/hexify (box-vector "bob-secret-key"))]
     (is (thrown? java.lang.ClassCastException
-                 (b/encrypt hex-public-key hex-private-key nonce plaintext))
-        "encrypt does not accept strings")))
+                 (b/encrypt hex-pk hex-sk nonce ptext)))))
