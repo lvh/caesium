28a730bce04b75c83600c335ccb630fb47d5b99f
Laurens Van Houtven <_@lvh.io>
2016-05-07 11:05:18 -0500

    Add appropriate warning signs

--- src/caesium/crypto/scalarmult.clj
+++ src/caesium/crypto/scalarmult.clj
@@ -1,5 +1,8 @@
 (ns caesium.crypto.scalarmult
-  "Scalar multiplication."
   (:require [caesium.binding :refer [defconsts]]))
+  "**DANGER** This namespace consists of low-level details that you should
+  not use unless you know what you are doing.
+
+  Scalar multiplication."
 
 (defconsts [bytes scalarbytes primitive])
