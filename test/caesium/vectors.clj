dd2afc9e967fab28f7ebd8e3205445bc536ebec5
Laurens Van Houtven <_@lvh.io>
2016-05-07 16:58:28 -0500

    Fix import syntax

--- test/caesium/vectors.clj
+++ test/caesium/vectors.clj
@@ -1,8 +1,7 @@
 (ns caesium.vectors
-  (:require
-   [clojure.java.io :as io]
-   [caesium.util :as u]
-   [clojure.string :as s]))
+  (:require [caesium.util :as u]
+            [clojure.java.io :as io]
+            [clojure.string :as s]))
 
 (def hex-resource
   "Gets a named resource in hex format; returns its contents as a byte
