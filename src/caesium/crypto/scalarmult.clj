3fd3c9bae2dc231bad89feb09d229c3a0e55400d
Laurens Van Houtven <_@lvh.io>
2016-05-07 11:51:04 -0500

    Hint at the namespace they might want instead

--- src/caesium/crypto/scalarmult.clj
+++ src/caesium/crypto/scalarmult.clj
@@ -1,7 +1,8 @@
 (ns caesium.crypto.scalarmult
   (:require [caesium.binding :refer [defconsts]]))
-  "**DANGER** This namespace consists of low-level details that you should
-  not use unless you know what you are doing.
+  "**DANGER** This namespace consists of low-level details that you
+  should not use unless you know what you are doing. You probably
+  want [[caesium.crypto.box]] instead.
 
   Scalar multiplication."
 
