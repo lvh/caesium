c2fb2333199c723f8bbf5e80c2d7d5af6d930078
Laurens Van Houtven <_@lvh.io>
2016-05-07 11:11:11 -0500

    Add a test for int->scalar

--- test/caesium/crypto/scalarmult_test.clj
+++ test/caesium/crypto/scalarmult_test.clj
@@ -1,8 +1,15 @@
 (ns caesium.crypto.scalarmult-test
   (:require [caesium.crypto.scalarmult :as s]
-            [clojure.test :refer [deftest is]]))
+            [clojure.test :refer [deftest is are]]
+            [caesium.util :as u]))
 
 (deftest consts-tests
   (is (= 32 s/bytes))
   (is (= 32 s/scalarbytes))
   (is (= "curve25519" s/primitive)))
+(deftest int->scalar-test
+  (are [n expected] (u/array-eq expected (u/n->bytes 32 n))
+    0 (byte-array 32)
+    0M (byte-array 32)
+    1000000000000 (byte-array (into (vec (repeat 27 0))
+                                    [-24 -44 -91 16 0]))))
