2d836e9be9deabffbe0c4b3a82b4315cb7c1930a
Laurens Van Houtven <_@lvh.io>
2016-05-15 12:19:30 -0500

    Reorganize tests

--- test/caesium/crypto/sign_test.clj
+++ test/caesium/crypto/sign_test.clj
@@ -11,6 +11,13 @@
  s/secretkeybytes 64
  s/primitive "ed25519")
 
+(deftest generate-signing-keys-test
+  (let [kp1 (s/generate-signing-keys)
+        kp2 (s/generate-signing-keys)]
+    (is (not (u/array-eq (:public kp1) (:public kp2))))
+    (is (not (u/array-eq (:secret kp1) (:secret kp2)))))
+  (is (u/array-eq public (:public (s/generate-signing-keys secret)))))
+
 ;; Test values taken from Kalium's suite
 (def secret
   (u/unhexify "b18e1d0045995ec3d010c387ccfeb984d783af8fbb0f40fa7db126d889f6dadd"))
@@ -20,13 +27,6 @@
   (u/unhexify "916c7d1d268fc0e77c1bef238432573c39be577bbea0998936add2b50a653171ce18a542b0b7f96c1691a3be6031522894a8634183eda38798a0c5d5d79fbd01dd04a8646d71873b77b221998a81922d8105f892316369d5224c9983372d2313c6b1f4556ea26ba49d46e8b561e0fc76633ac9766e68e21fba7edca93c4c7460376d7f3ac22ff372c18f613f2ae2e856af40"))
 (def signature
   (u/unhexify "6bd710a368c1249923fc7a1610747403040f0cc30815a00f9ff548a896bbda0b4eb2ca19ebcf917f0f34200a9edbad3901b64ab09cc5ef7b9bcc3c40c0ff7509"))
-
-(deftest generate-signing-keys-test
-  (let [kp1 (s/generate-signing-keys)
-        kp2 (s/generate-signing-keys)]
-    (is (not (u/array-eq (:public kp1) (:public kp2))))
-    (is (not (u/array-eq (:secret kp1) (:secret kp2)))))
-  (is (u/array-eq public (:public (s/generate-signing-keys secret)))))
 
 (deftest sign-test
   (testing "Can sign a message"
