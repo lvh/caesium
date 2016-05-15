b6a7ba0e6c4659b1e42202b9a14d0e84faa5596a
Laurens Van Houtven <_@lvh.io>
2016-05-15 15:41:26 -0500

    Separate pair-from-secret

--- test/caesium/crypto/sign_test.clj
+++ test/caesium/crypto/sign_test.clj
@@ -15,8 +15,7 @@
   (let [kp1 (s/generate-signing-keys)
         kp2 (s/generate-signing-keys)]
     (is (not (u/array-eq (:public kp1) (:public kp2))))
-    (is (not (u/array-eq (:secret kp1) (:secret kp2)))))
-  (is (u/array-eq public (:public (s/generate-signing-keys secret)))))
+    (is (not (u/array-eq (:secret kp1) (:secret kp2))))))
 
 ;; Test values taken from Kalium's suite
 (def secret
@@ -27,6 +26,9 @@
   (u/unhexify "916c7d1d268fc0e77c1bef238432573c39be577bbea0998936add2b50a653171ce18a542b0b7f96c1691a3be6031522894a8634183eda38798a0c5d5d79fbd01dd04a8646d71873b77b221998a81922d8105f892316369d5224c9983372d2313c6b1f4556ea26ba49d46e8b561e0fc76633ac9766e68e21fba7edca93c4c7460376d7f3ac22ff372c18f613f2ae2e856af40"))
 (def signature
   (u/unhexify "6bd710a368c1249923fc7a1610747403040f0cc30815a00f9ff548a896bbda0b4eb2ca19ebcf917f0f34200a9edbad3901b64ab09cc5ef7b9bcc3c40c0ff7509"))
+
+(deftest pair-from-secret-test
+  (is (u/array-eq public (:public (s/generate-signing-keys secret)))))
 
 (deftest detached-sign-test
   (is (u/array-eq signature (s/sign secret message))))
