05345bd58701c2fd90bc588946a1281597dff713
Laurens Van Houtven <_@lvh.io>
2016-05-20 22:29:20 -0500

    Whole bunch of tests

--- test/caesium/magicnonce/secretbox_test.clj
+++ test/caesium/magicnonce/secretbox_test.clj
@@ -3,6 +3,7 @@
             [caesium.crypto.secretbox :as s]
             [caesium.crypto.secretbox-test :as st]
             [clojure.test :refer [deftest is]]
+            [caesium.randombytes :as r]
             [caesium.util :as u]))
 
 (deftest xor-test
@@ -18,10 +19,43 @@
 
 (deftest secretbox-pfx-test
   (let [nonce (byte-array (range s/noncebytes))
-        ctext (ms/secretbox-pfx st/ptext nonce st/secret-key)]
-    (is (= (+ s/noncebytes
-              (alength ^bytes st/ptext)
-              s/macbytes)
+        ctext (ms/secretbox-pfx st/ptext nonce st/secret-key)
+        ptextlen (alength ^bytes st/ptext)]
+    (is (= (+ s/noncebytes ptextlen s/macbytes)
            (alength ^bytes ctext)))
     (is (= (range s/noncebytes)
-           (take s/noncebytes ctext)))))
+           (take s/noncebytes ctext)))
+
+    (let [out (byte-array ptextlen)]
+      (ms/decrypt-to-buf! out st/secret-key ctext)
+      (is (u/array-eq st/ptext out)))
+
+    (let [out (byte-array ptextlen)
+          forgery (r/randombytes (alength ^bytes out))]
+      (is (thrown-with-msg?
+           RuntimeException #"Ciphertext verification failed"
+           (ms/decrypt-to-buf! out st/secret-key forgery))))
+
+    (is (u/array-eq st/ptext (ms/decrypt st/secret-key ctext)))
+
+    (let [forgery (r/randombytes (alength ^bytes out))]
+      (is (thrown-with-msg
+           RuntimeException #"Ciphertext verification failed"
+           (ms/decrypt st/secret-key forgery))))
+
+    (let [out (byte-array ptextlen)]
+      (ms/open-to-buf! out ctext st/secret-key)
+      (is (u/array-eq st/ptext out)))
+
+    (let [out (byte-array ptextlen)
+          forgery (r/randombytes (alength ^bytes out))]
+      (is (thrown-with-msg?
+           RuntimeException #"Ciphertext verification failed"
+           (ms/open-to-buf! out forgery st/secret-key))))
+
+    (is (u/array-eq st/ptext (ms/open ctext st/secret-key)))
+
+    (let [forgery (r/randombytes (alength ^bytes out))]
+      (is (thrown-with-msg
+           RuntimeException #"Ciphertext verification failed"
+           (ms/open forgery st/secret-key))))))
