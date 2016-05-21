ced6762201c5ed4460522c88b151b202e6fc09fd
Laurens Van Houtven <_@lvh.io>
2016-05-20 22:05:49 -0500

    Add a test for secretbox-pfx

--- test/caesium/magicnonce/secretbox_test.clj
+++ test/caesium/magicnonce/secretbox_test.clj
@@ -1,5 +1,7 @@
 (ns caesium.magicnonce.secretbox-test
-  (:require [caesium.magicnonce.secretbox :as s]
+  (:require [caesium.magicnonce.secretbox :as ms]
+            [caesium.crypto.secretbox :as s]
+            [caesium.crypto.secretbox-test :as st]
             [clojure.test :refer [deftest is]]
             [caesium.util :as u]))
 
@@ -7,9 +9,17 @@
   (let [one (byte-array [1 0 1])
         two (byte-array [0 1 0])
         out (byte-array [0 0 0])]
-    (is (identical? (#'s/xor! out one two) out))
+    (is (identical? (#'ms/xor! out one two) out))
     (is (u/array-eq (byte-array [1 1 1]) out)))
   (let [one (byte-array [1 0 1])
         two (byte-array [0 1 0])]
-    (is (identical? (#'s/xor-inplace! one two) one))
+    (is (identical? (#'ms/xor-inplace! one two) one))
     (is (u/array-eq (byte-array [1 1 1]) one))))
+
+(deftest secretbox-pfx-test
+  (let [nonce (byte-array (range s/noncebytes))
+        ctext (ms/secretbox-pfx st/ptext nonce st/secret-key)]
+    (is (= (+ s/noncebytes
+              (alength ^bytes st/ptext)
+              s/macbytes)
+           (alength ^bytes ctext)))))
