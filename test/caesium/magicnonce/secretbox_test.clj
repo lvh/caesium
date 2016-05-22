8020a2b0f8ea9c5b9f232bca625bc809830bbab1
Laurens Van Houtven <_@lvh.io>
2016-05-22 10:14:30 -0500

    Add tests for secretbox-rnd

--- test/caesium/magicnonce/secretbox_test.clj
+++ test/caesium/magicnonce/secretbox_test.clj
@@ -65,3 +65,54 @@
       (is (thrown-with-msg?
            RuntimeException #"Ciphertext verification failed"
            (ms/open forgery st/secret-key))))))
+
+(deftest secretbox-rnd-test
+  (let [ctext (ms/secretbox-rnd st/ptext st/secret-key)
+        ptextlen (alength ^bytes st/ptext)]
+    (is (= (+ s/noncebytes ptextlen s/macbytes)
+           (alength ^bytes ctext)))
+
+    (let [out (byte-array ptextlen)]
+      (ms/decrypt-to-buf! out st/secret-key ctext)
+      (is (u/array-eq st/ptext out)))
+
+    (let [out (byte-array ptextlen)
+          forgery (r/randombytes (alength ^bytes out))]
+      (is (thrown-with-msg?
+           RuntimeException #"Ciphertext verification failed"
+           (ms/decrypt-to-buf! out st/secret-key forgery))))
+
+    (is (u/array-eq st/ptext (ms/decrypt st/secret-key ctext)))
+
+    (let [forgery (r/randombytes (alength ^bytes ctext))]
+      (is (thrown-with-msg?
+           RuntimeException #"Ciphertext verification failed"
+           (ms/decrypt st/secret-key forgery))))
+
+    (let [out (byte-array ptextlen)]
+      (ms/open-to-buf! out ctext st/secret-key)
+      (is (u/array-eq st/ptext out)))
+
+    (let [out (byte-array ptextlen)
+          forgery (r/randombytes (alength ^bytes out))]
+      (is (thrown-with-msg?
+           RuntimeException #"Ciphertext verification failed"
+           (ms/open-to-buf! out forgery st/secret-key))))
+
+    (is (u/array-eq st/ptext (ms/open ctext st/secret-key)))
+
+    (let [forgery (r/randombytes (alength ^bytes ctext))]
+      (is (thrown-with-msg?
+           RuntimeException #"Ciphertext verification failed"
+           (ms/open forgery st/secret-key)))))
+
+  (let [c1 (ms/secretbox-rnd st/ptext st/secret-key)
+        c2 (ms/secretbox-rnd st/ptext st/secret-key)]
+    (is (not (u/array-eq c1 c2)))
+    (is (not= (take s/noncebytes c1) (take s/noncebytes c2))))
+
+  (with-redefs [ms/random-nonce! (constantly (byte-array (range s/noncebytes)))]
+    (let [c1 (ms/secretbox-rnd st/ptext st/secret-key)
+          c2 (ms/secretbox-rnd st/ptext st/secret-key)]
+      (is (u/array-eq c1 c2))
+      (is (= (range s/noncebytes) (take s/noncebytes c1) (take s/noncebytes c2))))))
