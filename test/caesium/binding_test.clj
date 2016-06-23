5475126e54ddcddea6d23f06dc239f9f907a5d9c
lvh <_@lvh.io>
2016-06-23 16:48:35 -0500

    Also have an interesting test

--- test/caesium/binding_test.clj
+++ test/caesium/binding_test.clj
@@ -5,4 +5,48 @@
 (deftest permuted-byte-types-test
   (is (= '[[^long ^{size_t {}} crypto_secretbox_keybytes []]]
          (#'b/permuted-byte-types
-          '[^long ^{size_t {}} crypto_secretbox_keybytes []]))))
+          '[^long ^{size_t {}} crypto_secretbox_keybytes []])))
+  (is (= '[[bytes bytes long bytes bytes]
+           [bytes bytes long bytes java.nio.ByteBuffer]
+           [bytes bytes long java.nio.ByteBuffer bytes]
+           [bytes bytes long java.nio.ByteBuffer java.nio.ByteBuffer]
+           [bytes java.nio.ByteBuffer long bytes bytes]
+           [bytes java.nio.ByteBuffer long bytes java.nio.ByteBuffer]
+           [bytes java.nio.ByteBuffer long java.nio.ByteBuffer bytes]
+           [bytes
+            java.nio.ByteBuffer
+            long
+            java.nio.ByteBuffer
+            java.nio.ByteBuffer]
+           [java.nio.ByteBuffer bytes long bytes bytes]
+           [java.nio.ByteBuffer bytes long bytes java.nio.ByteBuffer]
+           [java.nio.ByteBuffer bytes long java.nio.ByteBuffer bytes]
+           [java.nio.ByteBuffer
+            bytes
+            long
+            java.nio.ByteBuffer
+            java.nio.ByteBuffer]
+           [java.nio.ByteBuffer java.nio.ByteBuffer long bytes bytes]
+           [java.nio.ByteBuffer
+            java.nio.ByteBuffer
+            long
+            bytes
+            java.nio.ByteBuffer]
+           [java.nio.ByteBuffer
+            java.nio.ByteBuffer
+            long
+            java.nio.ByteBuffer
+            bytes]
+           [java.nio.ByteBuffer
+            java.nio.ByteBuffer
+            long
+            java.nio.ByteBuffer
+            java.nio.ByteBuffer]]
+         (mapv (fn [[_ args]] (mapv (comp :tag meta) args))
+              (#'b/permuted-byte-types
+               '[^int crypto_secretbox_easy
+                 [^bytes ^{Pinned {}} c
+                  ^bytes ^{Pinned {}} m
+                  ^long ^{LongLong {}} mlen
+                  ^bytes ^{Pinned {}} n
+                  ^bytes ^{Pinned {}} k]])))))
