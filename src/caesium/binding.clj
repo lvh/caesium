b8bfbb19f071cd946715f03a96be0a3655e37e9e
lvh <_@lvh.io>
2016-06-24 09:13:57 -0500

    Expose all secretbox_easy variants

--- src/caesium/binding.clj
+++ src/caesium/binding.clj
@@ -41,187 +41,182 @@
   This has to be a vec and not a map, because the same key (symbol,
   method name) might occur with multiple values (e.g. when binding the
   same char* fn with different JVM byte types)."
-  '[[^int sodium_init []]
-    [^String sodium_version_string []]
+  (concat
+   '[[^int sodium_init []]
+     [^String sodium_version_string []]
 
-    [^void randombytes
-     [^bytes ^{Pinned {}} buf
-      ^long ^{LongLong {}} buflen]]
+     [^void randombytes
+      [^bytes ^{Pinned {}} buf
+       ^long ^{LongLong {}} buflen]]
 
-    [^long ^{size_t {}} crypto_secretbox_keybytes []]
-    [^long ^{size_t {}} crypto_secretbox_noncebytes []]
-    [^long ^{size_t {}} crypto_secretbox_macbytes []]
-    [^String crypto_secretbox_primitive []]
+     [^long ^{size_t {}} crypto_secretbox_keybytes []]
+     [^long ^{size_t {}} crypto_secretbox_noncebytes []]
+     [^long ^{size_t {}} crypto_secretbox_macbytes []]
+     [^String crypto_secretbox_primitive []]]
+   (permuted-byte-types
+    '[^int crypto_secretbox_easy
+      [^bytes ^{Pinned {}} c
+       ^bytes ^{Pinned {}} m
+       ^long ^{LongLong {}} mlen
+       ^bytes ^{Pinned {}} n
+       ^bytes ^{Pinned {}} k]])
+   '[[^int crypto_secretbox_open_easy
+      [^bytes ^{Pinned {}} m
+       ^bytes ^{Pinned {}} c
+       ^long ^{LongLong {}} clen
+       ^bytes ^{Pinned {}} n
+       ^bytes ^{Pinned {}} k]]
+     [^int crypto_secretbox_open_easy
+      [^bytes ^{Pinned {}} m
+       ^java.nio.ByteBuffer ^{Pinned {}} c
+       ^long ^{LongLong {}} clen
+       ^java.nio.ByteBuffer ^{Pinned {}} n
+       ^bytes ^{Pinned {}} k]]
+     [^int crypto_secretbox_detached
+      [^bytes ^{Pinned {}} c
+       ^bytes ^{Pinned {}} mac
+       ^bytes ^{Pinned {}} m
+       ^long ^{LongLong {}} mlen
+       ^bytes ^{Pinned {}} n
+       ^bytes ^{Pinned {}} k]]
+     [^int crypto_secretbox_open_detached
+      [^bytes ^{Pinned {}} m
+       ^bytes ^{Pinned {}} c
+       ^bytes ^{Pinned {}} mac
+       ^long ^{LongLong {}} clen
+       ^bytes ^{Pinned {}} n
+       ^bytes ^{Pinned {}} k]]
 
-    [^int crypto_secretbox_easy
-     [^bytes ^{Pinned {}} c
-      ^bytes ^{Pinned {}} m
-      ^long ^{LongLong {}} mlen
-      ^bytes ^{Pinned {}} n
-      ^bytes ^{Pinned {}} k]]
-    [^int crypto_secretbox_easy
-     [^java.nio.ByteBuffer ^{Pinned {}} c
-      ^bytes ^{Pinned {}} m
-      ^long ^{LongLong {}} mlen
-      ^bytes ^{Pinned {}} n
-      ^bytes ^{Pinned {}} k]]
-    [^int crypto_secretbox_open_easy
-     [^bytes ^{Pinned {}} m
-      ^bytes ^{Pinned {}} c
-      ^long ^{LongLong {}} clen
-      ^bytes ^{Pinned {}} n
-      ^bytes ^{Pinned {}} k]]
-    [^int crypto_secretbox_open_easy
-     [^bytes ^{Pinned {}} m
-      ^java.nio.ByteBuffer ^{Pinned {}} c
-      ^long ^{LongLong {}} clen
-      ^java.nio.ByteBuffer ^{Pinned {}} n
-      ^bytes ^{Pinned {}} k]]
-    [^int crypto_secretbox_detached
-     [^bytes ^{Pinned {}} c
-      ^bytes ^{Pinned {}} mac
-      ^bytes ^{Pinned {}} m
-      ^long ^{LongLong {}} mlen
-      ^bytes ^{Pinned {}} n
-      ^bytes ^{Pinned {}} k]]
-    [^int crypto_secretbox_open_detached
-     [^bytes ^{Pinned {}} m
-      ^bytes ^{Pinned {}} c
-      ^bytes ^{Pinned {}} mac
-      ^long ^{LongLong {}} clen
-      ^bytes ^{Pinned {}} n
-      ^bytes ^{Pinned {}} k]]
+     [^long ^{size_t {}} crypto_box_seedbytes []]
+     [^long ^{size_t {}} crypto_box_publickeybytes []]
+     [^long ^{size_t {}} crypto_box_secretkeybytes []]
+     [^long ^{size_t {}} crypto_box_noncebytes []]
+     [^long ^{size_t {}} crypto_box_macbytes []]
+     [^String ^{size_t {}} crypto_box_primitive []]
 
-    [^long ^{size_t {}} crypto_box_seedbytes []]
-    [^long ^{size_t {}} crypto_box_publickeybytes []]
-    [^long ^{size_t {}} crypto_box_secretkeybytes []]
-    [^long ^{size_t {}} crypto_box_noncebytes []]
-    [^long ^{size_t {}} crypto_box_macbytes []]
-    [^String ^{size_t {}} crypto_box_primitive []]
+     [^int crypto_box_seed_keypair
+      [^bytes ^{Pinned {}} pk
+       ^bytes ^{Pinned {}} sk
+       ^bytes ^{Pinned {}} seed]]
+     [^int crypto_box_keypair
+      [^bytes ^{Pinned {}} pk
+       ^bytes ^{Pinned {}} sk]]
+     [^int crypto_box_easy
+      [^bytes ^{Pinned {}} c
+       ^bytes ^{Pinned {}} m
+       ^long ^{LongLong {}} mlen
+       ^bytes ^{Pinned {}} n
+       ^bytes ^{Pinned {}} pk
+       ^bytes ^{Pinned {}} sk]]
+     [^int crypto_box_open_easy
+      [^bytes ^{Pinned {}} m
+       ^bytes ^{Pinned {}} c
+       ^long ^{LongLong {}} clen
+       ^bytes ^{Pinned {}} n
+       ^bytes ^{Pinned {}} pk
+       ^bytes ^{Pinned {}} sk]]
 
-    [^int crypto_box_seed_keypair
-     [^bytes ^{Pinned {}} pk
-      ^bytes ^{Pinned {}} sk
-      ^bytes ^{Pinned {}} seed]]
-    [^int crypto_box_keypair
-     [^bytes ^{Pinned {}} pk
-      ^bytes ^{Pinned {}} sk]]
-    [^int crypto_box_easy
-     [^bytes ^{Pinned {}} c
-      ^bytes ^{Pinned {}} m
-      ^long ^{LongLong {}} mlen
-      ^bytes ^{Pinned {}} n
-      ^bytes ^{Pinned {}} pk
-      ^bytes ^{Pinned {}} sk]]
-    [^int crypto_box_open_easy
-     [^bytes ^{Pinned {}} m
-      ^bytes ^{Pinned {}} c
-      ^long ^{LongLong {}} clen
-      ^bytes ^{Pinned {}} n
-      ^bytes ^{Pinned {}} pk
-      ^bytes ^{Pinned {}} sk]]
+     [^long ^{size_t {}} crypto_sign_bytes []]
+     [^long ^{size_t {}} crypto_sign_seedbytes []]
+     [^long ^{size_t {}} crypto_sign_publickeybytes []]
+     [^long ^{size_t {}} crypto_sign_secretkeybytes []]
+     [^String crypto_sign_primitive []]
 
-    [^long ^{size_t {}} crypto_sign_bytes []]
-    [^long ^{size_t {}} crypto_sign_seedbytes []]
-    [^long ^{size_t {}} crypto_sign_publickeybytes []]
-    [^long ^{size_t {}} crypto_sign_secretkeybytes []]
-    [^String crypto_sign_primitive []]
+     [^int crypto_sign_keypair
+      [^bytes ^{Pinned {}} pk
+       ^bytes ^{Pinned {}} sk]]
+     [^int crypto_sign_seed_keypair
+      [^bytes ^{Pinned {}} pk
+       ^bytes ^{Pinned {}} sk
+       ^bytes ^{Pinned {}} seed]]
+     [^int crypto_sign
+      [^bytes ^{Pinned {}} sm
+       ^jnr.ffi.byref.LongLongByReference smlen_p
+       ^bytes ^{Pinned {}} m
+       ^long ^{LongLong {}} mlen
+       ^bytes ^{Pinned {}} sk]]
+     [^int crypto_sign_open
+      [^bytes ^{Pinned {}} m
+       ^jnr.ffi.byref.LongLongByReference mlen_p
+       ^bytes ^{Pinned {}} sm
+       ^long ^{LongLong {}} smlen
+       ^bytes ^{Pinned {}} pk]]
+     [^int crypto_sign_detached
+      [^bytes ^{Pinned {}} sig
+       ^jnr.ffi.byref.LongLongByReference siglen_p
+       ^bytes ^{Pinned {}} m
+       ^long ^{LongLong {}} mlen
+       ^bytes ^{Pinned {}} sk]]
+     [^int crypto_sign_verify_detached
+      [^bytes ^{Pinned {}} sig
+       ^bytes ^{Pinned {}} m
+       ^long ^{LongLong {}} mlen
+       ^bytes ^{Pinned {}} pk]]
 
-    [^int crypto_sign_keypair
-     [^bytes ^{Pinned {}} pk
-      ^bytes ^{Pinned {}} sk]]
-    [^int crypto_sign_seed_keypair
-     [^bytes ^{Pinned {}} pk
-      ^bytes ^{Pinned {}} sk
-      ^bytes ^{Pinned {}} seed]]
-    [^int crypto_sign
-     [^bytes ^{Pinned {}} sm
-      ^jnr.ffi.byref.LongLongByReference smlen_p
-      ^bytes ^{Pinned {}} m
-      ^long ^{LongLong {}} mlen
-      ^bytes ^{Pinned {}} sk]]
-    [^int crypto_sign_open
-     [^bytes ^{Pinned {}} m
-      ^jnr.ffi.byref.LongLongByReference mlen_p
-      ^bytes ^{Pinned {}} sm
-      ^long ^{LongLong {}} smlen
-      ^bytes ^{Pinned {}} pk]]
-    [^int crypto_sign_detached
-     [^bytes ^{Pinned {}} sig
-      ^jnr.ffi.byref.LongLongByReference siglen_p
-      ^bytes ^{Pinned {}} m
-      ^long ^{LongLong {}} mlen
-      ^bytes ^{Pinned {}} sk]]
-    [^int crypto_sign_verify_detached
-     [^bytes ^{Pinned {}} sig
-      ^bytes ^{Pinned {}} m
-      ^long ^{LongLong {}} mlen
-      ^bytes ^{Pinned {}} pk]]
+     [^long ^{size_t {}} crypto_generichash_bytes_min []]
+     [^long ^{size_t {}} crypto_generichash_bytes_max []]
+     [^long ^{size_t {}} crypto_generichash_bytes []]
+     [^long ^{size_t {}} crypto_generichash_keybytes_min []]
+     [^long ^{size_t {}} crypto_generichash_keybytes_max []]
+     [^long ^{size_t {}} crypto_generichash_keybytes []]
+     [^String crypto_generichash_primitive []]
+     [^int crypto_generichash
+      [^bytes ^{Pinned {}} buf
+       ^long ^{LongLong {}} buflen
+       ^bytes ^{Pinned {}} msg
+       ^long ^{LongLong {}} msglen
+       ^bytes ^{Pinned {}} key
+       ^long ^{LongLong {}} keylen]]
 
-    [^long ^{size_t {}} crypto_generichash_bytes_min []]
-    [^long ^{size_t {}} crypto_generichash_bytes_max []]
-    [^long ^{size_t {}} crypto_generichash_bytes []]
-    [^long ^{size_t {}} crypto_generichash_keybytes_min []]
-    [^long ^{size_t {}} crypto_generichash_keybytes_max []]
-    [^long ^{size_t {}} crypto_generichash_keybytes []]
-    [^String crypto_generichash_primitive []]
-    [^int crypto_generichash
-     [^bytes ^{Pinned {}} buf
-      ^long ^{LongLong {}} buflen
-      ^bytes ^{Pinned {}} msg
-      ^long ^{LongLong {}} msglen
-      ^bytes ^{Pinned {}} key
-      ^long ^{LongLong {}} keylen]]
+     [^long ^{size_t {}} crypto_generichash_blake2b_bytes_min []]
+     [^long ^{size_t {}} crypto_generichash_blake2b_bytes_max []]
+     [^long ^{size_t {}} crypto_generichash_blake2b_bytes []]
+     [^long ^{size_t {}} crypto_generichash_blake2b_keybytes_min []]
+     [^long ^{size_t {}} crypto_generichash_blake2b_keybytes_max []]
+     [^long ^{size_t {}} crypto_generichash_blake2b_keybytes []]
+     [^long ^{size_t {}} crypto_generichash_blake2b_saltbytes []]
+     [^long ^{size_t {}} crypto_generichash_blake2b_personalbytes []]
+     [^long ^{size_t {}} crypto_generichash_blake2b_statebytes []]
+     [^int crypto_generichash_blake2b
+      [^bytes ^{Pinned {}} buf
+       ^long ^{LongLong {}} buflen
+       ^bytes ^{Pinned {}} msg
+       ^long ^{LongLong {}} msglen
+       ^bytes ^{Pinned {}} key
+       ^long ^{LongLong {}} keylen]]
+     [^int crypto_generichash_blake2b_salt_personal
+      [^bytes ^{Pinned {}} buf
+       ^long ^{LongLong {}} buflen
+       ^bytes ^{Pinned {}} msg
+       ^long ^{LongLong {}} msglen
+       ^bytes ^{Pinned {}} key
+       ^long ^{LongLong {}} keylen
+       ^bytes ^{Pinned {}} salt
+       ^bytes ^{Pinned {}} personal]]
 
-    [^long ^{size_t {}} crypto_generichash_blake2b_bytes_min []]
-    [^long ^{size_t {}} crypto_generichash_blake2b_bytes_max []]
-    [^long ^{size_t {}} crypto_generichash_blake2b_bytes []]
-    [^long ^{size_t {}} crypto_generichash_blake2b_keybytes_min []]
-    [^long ^{size_t {}} crypto_generichash_blake2b_keybytes_max []]
-    [^long ^{size_t {}} crypto_generichash_blake2b_keybytes []]
-    [^long ^{size_t {}} crypto_generichash_blake2b_saltbytes []]
-    [^long ^{size_t {}} crypto_generichash_blake2b_personalbytes []]
-    [^long ^{size_t {}} crypto_generichash_blake2b_statebytes []]
-    [^int crypto_generichash_blake2b
-     [^bytes ^{Pinned {}} buf
-      ^long ^{LongLong {}} buflen
-      ^bytes ^{Pinned {}} msg
-      ^long ^{LongLong {}} msglen
-      ^bytes ^{Pinned {}} key
-      ^long ^{LongLong {}} keylen]]
-    [^int crypto_generichash_blake2b_salt_personal
-     [^bytes ^{Pinned {}} buf
-      ^long ^{LongLong {}} buflen
-      ^bytes ^{Pinned {}} msg
-      ^long ^{LongLong {}} msglen
-      ^bytes ^{Pinned {}} key
-      ^long ^{LongLong {}} keylen
-      ^bytes ^{Pinned {}} salt
-      ^bytes ^{Pinned {}} personal]]
+     [^long ^{size_t {}} crypto_hash_sha256_bytes []]
+     [^int crypto_hash_sha256
+      [^bytes ^{Pinned {}} buf
+       ^bytes ^{Pinned {}} msg
+       ^long ^{LongLong {}} msglen]]
 
-    [^long ^{size_t {}} crypto_hash_sha256_bytes []]
-    [^int crypto_hash_sha256
-     [^bytes ^{Pinned {}} buf
-      ^bytes ^{Pinned {}} msg
-      ^long ^{LongLong {}} msglen]]
+     [^long ^{size_t {}} crypto_hash_sha512_bytes []]
+     [^int crypto_hash_sha512
+      [^bytes ^{Pinned {}} buf
+       ^bytes ^{Pinned {}} msg
+       ^long ^{LongLong {}} msglen]]
 
-    [^long ^{size_t {}} crypto_hash_sha512_bytes []]
-    [^int crypto_hash_sha512
-     [^bytes ^{Pinned {}} buf
-      ^bytes ^{Pinned {}} msg
-      ^long ^{LongLong {}} msglen]]
+     [^long ^{size_t {}} crypto_scalarmult_bytes []]
+     [^long ^{size_t {}} crypto_scalarmult_scalarbytes []]
+     [^String crypto_scalarmult_primitive []]
 
-    [^long ^{size_t {}} crypto_scalarmult_bytes []]
-    [^long ^{size_t {}} crypto_scalarmult_scalarbytes []]
-    [^String crypto_scalarmult_primitive []]
-
-    [^int crypto_scalarmult_base
-     [^bytes ^{Pinned {}} q
-      ^bytes ^{Pinned {}} n]]
-    [^int crypto_scalarmult
-     [^bytes ^{Pinned {}} q
-      ^bytes ^{Pinned {}} n
-      ^bytes ^{Pinned {}} p]]])
+     [^int crypto_scalarmult_base
+      [^bytes ^{Pinned {}} q
+       ^bytes ^{Pinned {}} n]]
+     [^int crypto_scalarmult
+      [^bytes ^{Pinned {}} q
+       ^bytes ^{Pinned {}} n
+       ^bytes ^{Pinned {}} p]]]))
 
 (defmacro ^:private defsodium
   []
