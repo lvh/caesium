95783e421cd42f1cfafa021c74cf4a9ac52a4b2c
lvh <_@lvh.io>
2016-06-25 14:00:49 -0400

    secretbox-easy experiments with byte buffers

--- src/caesium/crypto/secretbox.clj
+++ src/caesium/crypto/secretbox.clj
@@ -1,9 +1,84 @@
 (ns caesium.crypto.secretbox
   "Bindings to the secretbox secret-key authenticated encryption scheme."
   (:require [caesium.binding :refer [sodium defconsts]]
-            [caesium.util :as u]))
+            [caesium.util :as u]
+            [caesium.bytes-conv :as bc])
+  (:import [java.nio ByteBuffer]))
 
 (defconsts [keybytes noncebytes macbytes primitive])
+
+(defn secretbox-easy-to-direct-byte-bufs-with-macros!
+  "Encrypt with `crypto_secretbox_easy`.
+
+  All arguments will be converted to byte buffers. If this involves
+  allocating a new byte buffer, that byte buffer will be allocated as
+  a direct byte buffer. This is the case whenever an argument is not a
+  byte buffer already; including when it is a byte array. You only
+  want this to manage the output byte buffers yourself. Otherwise, you
+  want [[secretbox-easy]]. "
+  [out msg nonce key]
+  (let [^ByteBuffer out (bc/->direct-byte-buf-macro out)
+        ^ByteBuffer msg (bc/->direct-byte-buf-macro msg)
+        ^ByteBuffer nonce (bc/->direct-byte-buf-macro nonce)
+        ^ByteBuffer key (bc/->direct-byte-buf-macro key)]
+    (.crypto_secretbox_easy sodium out msg (.remaining msg) nonce key)
+    out))
+
+(defn secretbox-easy-to-direct-byte-bufs!
+  "Encrypt with `crypto_secretbox_easy`.
+
+  All arguments will be converted to byte buffers. If this involves
+  allocating a new byte buffer, that byte buffer will be allocated as
+  a direct byte buffer. This is the case whenever an argument is not a
+  byte buffer already; including when it is a byte array. You only
+  want this to manage the output byte buffers yourself. Otherwise, you
+  want [[secretbox-easy]]. "
+  [out msg nonce key]
+  (let [^ByteBuffer out (bc/->direct-byte-buf out)
+        ^ByteBuffer msg (bc/->direct-byte-buf msg)
+        ^ByteBuffer nonce (bc/->direct-byte-buf nonce)
+        ^ByteBuffer key (bc/->direct-byte-buf key)]
+    (.crypto_secretbox_easy sodium out msg (.remaining msg) nonce key)
+    out))
+
+(defn secretbox-easy-to-indirect-byte-bufs-with-macros!
+  "Encrypt with `crypto_secretbox_easy`.
+
+  All arguments will be converted to byte buffers. If this involves
+  allocating a new byte buffer, that byte buffer will be allocated as
+  an indirect byte buffer. This is the case whenever an argument is not a
+  byte buffer already; including when it is a byte array. You only
+  want this to manage the output byte buffers yourself. Otherwise, you
+  want [[secretbox-easy]]. "
+  [out msg nonce key]
+  (let [^ByteBuffer out (bc/->indirect-byte-buf-macro out)
+        ^ByteBuffer msg (bc/->indirect-byte-buf-macro msg)
+        ^ByteBuffer nonce (bc/->indirect-byte-buf-macro nonce)
+        ^ByteBuffer key (bc/->indirect-byte-buf-macro key)]
+    (.crypto_secretbox_easy sodium out msg (.remaining msg) nonce key)
+    out))
+
+(defn secretbox-easy-to-indirect-byte-bufs!
+  "Encrypt with `crypto_secretbox_easy`.
+
+  All arguments will be converted to byte buffers. If this involves
+  allocating a new byte buffer, that byte buffer will be allocated as
+  an indirect byte buffer. This is the case whenever an argument is not a
+  byte buffer already; including when it is a byte array. You only
+  want this to manage the output byte buffers yourself. Otherwise, you
+  want [[secretbox-easy]]. "
+  [out msg nonce key]
+  (let [^ByteBuffer out (bc/->indirect-byte-buf out)
+        ^ByteBuffer msg (bc/->indirect-byte-buf msg)
+        ^ByteBuffer nonce (bc/->indirect-byte-buf nonce)
+        ^ByteBuffer key (bc/->indirect-byte-buf key)]
+    (.crypto_secretbox_easy sodium out msg (.remaining msg) nonce key)
+    out))
+
+(defn secretbox-easy-to-byte-bufs-nocast!
+  [^ByteBuffer out ^ByteBuffer msg ^ByteBuffer nonce ^ByteBuffer key]
+  (.crypto_secretbox_easy sodium out msg (.remaining msg) nonce key)
+  out)
 
 (defn secretbox-easy-to-buf!
   "Encrypt with `crypto_secretbox_easy` into the given byte array.
--- test/caesium/crypto/secretbox_benchmark.clj
+++ test/caesium/crypto/secretbox_benchmark.clj
@@ -1,5 +1,35 @@
 (ns caesium.crypto.secretbox-benchmark
   (:require [caesium.randombytes :refer [randombytes]]
             [caesium.crypto.secretbox :as s]
+            [caesium.bytes-conv :as bc]
             [clojure.test :refer [deftest]]
-            [criterium.core :refer [bench]]))
+            [criterium.core :refer [bench]]
+            [caesium.bench-utils :refer [fmt-bytes]])
+  (:import [java.nio ByteBuffer]))
+
+;; secretbox to-buf! macros vs fn vs no casts with indirect byte bufs
+
+(deftest ^:benchmark to-buf!-benchmarks
+  (let [key (randombytes s/keybytes)
+        sizes (map (partial bit-shift-left 1) [6 8 10 12 20 24])]
+    (println "secretbox to-buf! macros vs fn vs no casts with direct bufs")
+    (println "these bufs already exist, so there is no allocation")
+    (doseq [[size message] (map (juxt identity randombytes) sizes)
+            f [s/secretbox-easy-to-direct-byte-bufs-with-macros!
+               s/secretbox-easy-to-direct-byte-bufs!
+               s/secretbox-easy-to-byte-bufs-nocast!]]
+      (println f (fmt-bytes size))
+      (let [nonce (bc/->direct-byte-buf (randombytes s/noncebytes))
+            out (ByteBuffer/allocateDirect (+ s/macbytes size))]
+        (bench (f out message nonce key))))
+
+    (println "secretbox to-buf! macros vs fn vs no casts with indirect bufs")
+    (println "these bufs already exist, so there is no allocation")
+    (doseq [[size message] (map (juxt identity randombytes) sizes)
+            f [s/secretbox-easy-to-indirect-byte-bufs-with-macros!
+               s/secretbox-easy-to-indirect-byte-bufs!
+               s/secretbox-easy-to-byte-bufs-nocast!]]
+      (println f (fmt-bytes size))
+      (let [nonce (bc/->indirect-byte-buf (randombytes s/noncebytes))
+            out (ByteBuffer/allocate (+ s/macbytes size))]
+        (bench (f out message nonce key))))))
