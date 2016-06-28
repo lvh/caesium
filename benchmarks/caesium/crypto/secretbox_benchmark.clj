997ec7dbc3baa6dacc9b56610319a536fcff440d
lvh <_@lvh.io>
2016-06-28 15:51:11 -0500

    Add with-reflection benchmarks

--- benchmarks/caesium/crypto/secretbox_benchmark.clj
+++ benchmarks/caesium/crypto/secretbox_benchmark.clj
@@ -14,26 +14,41 @@
 
 (deftest ^:benchmark to-buf!-benchmarks
   (let [sizes (map (partial bit-shift-left 1) [6 8 10 12 20 24])]
-    (println "secretbox to-buf! macros vs fn vs no casts with direct bufs")
+    (println "secretbox to-buf! with direct bufs")
     (println "these bufs already exist, so there is no allocation")
     (doseq [[size msg] (map (juxt identity random-direct-byte-buf) sizes)
             f [s/secretbox-easy-to-direct-byte-bufs-with-macros!
                s/secretbox-easy-to-direct-byte-bufs!
-               s/secretbox-easy-to-byte-bufs-nocast!]]
+               s/secretbox-easy-to-byte-bufs-nocast!
+               s/secretbox-easy-refl!]]
       (println f (fmt-bytes size))
       (let [key (random-direct-byte-buf s/keybytes)
             nonce (random-direct-byte-buf s/noncebytes)
             out (ByteBuffer/allocateDirect (+ s/macbytes size))]
         (bench (f out msg nonce key))))
 
-    (println "secretbox to-buf! macros vs fn vs no casts with indirect bufs")
+    (println "secretbox to-buf! with indirect bufs")
     (println "these bufs already exist, so there is no allocation")
     (doseq [[size msg] (map (juxt identity random-indirect-byte-buf) sizes)
             f [s/secretbox-easy-to-indirect-byte-bufs-with-macros!
                s/secretbox-easy-to-indirect-byte-bufs!
-               s/secretbox-easy-to-byte-bufs-nocast!]]
+               s/secretbox-easy-to-byte-bufs-nocast!
+               s/secretbox-easy-refl!]]
       (println f (fmt-bytes size))
       (let [key (random-indirect-byte-buf s/keybytes)
             nonce (random-indirect-byte-buf s/noncebytes)
             out (ByteBuffer/allocate (+ s/macbytes size))]
+        (bench (f out msg nonce key))))
+
+    (println "secretbox to-buf! with byte arrays")
+    (println "these bufs already exist, so there is no allocation")
+    (doseq [[size msg] (map (juxt identity randombytes) sizes)
+            f [s/secretbox-easy-to-indirect-byte-bufs-with-macros!
+               s/secretbox-easy-to-indirect-byte-bufs!
+               s/secretbox-easy-to-byte-bufs-nocast!
+               s/secretbox-easy-refl!]]
+      (println f (fmt-bytes size))
+      (let [key (randombytes s/keybytes)
+            nonce (randombytes s/noncebytes)
+            out (randombytes (+ s/macbytes size))]
         (bench (f out msg nonce key))))))
