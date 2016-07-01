9e724fd6d64183afa7169064472c13255d0278ba
lvh <_@lvh.io>
2016-06-30 22:19:28 -0500

    Refactor benchmark

--- benchmarks/caesium/crypto/secretbox_benchmark.clj
+++ benchmarks/caesium/crypto/secretbox_benchmark.clj
@@ -7,45 +7,34 @@
             [caesium.bench-utils :refer [fmt-bytes]])
   (:import [java.nio ByteBuffer]))
 
-;; secretbox to-buf! macros vs fn vs no casts with indirect byte bufs
+(def sizes (map (partial bit-shift-left 1) [6 8 10 12 20 24]))
 
-(def random-direct-byte-buf (comp bc/->direct-byte-buf randombytes))
-(def random-indirect-byte-buf (comp bc/->indirect-byte-buf randombytes))
+(defmacro bench-secretnonce
+  [fs converter]
+  `(doseq [[size msg] (map (juxt identity (comp converter randombytes)) sizes)
+           f ~fs]
+     (let [rand-buf (comp converter randombytes)
+           key (rand-buf s/keybytes)
+           nonce (randbuf s/noncebytes)
+           out (randbuf (+ s/macbytes size))]
+       (println f (fmt-bytes size))
+       (spy (mapv type [out msg nonce key]))
+       (bench (f out msg nonce key)))))
 
 (deftest ^:benchmark to-buf!-benchmarks
-  (let [sizes (map (partial bit-shift-left 1) [6 8 10 12 20 24])]
-    (println "secretbox to-buf! with direct bufs")
-    (println "these bufs already exist, so there is no allocation")
-    (doseq [[size msg] (map (juxt identity random-direct-byte-buf) sizes)
-            f [s/secretbox-easy-to-direct-byte-bufs-with-macros!
-               s/secretbox-easy-to-direct-byte-bufs!
-               s/secretbox-easy-to-byte-bufs-nocast!
-               s/secretbox-easy-refl!]]
-      (let [key (random-direct-byte-buf s/keybytes)
-            nonce (random-direct-byte-buf s/noncebytes)
-            out (ByteBuffer/allocateDirect (+ s/macbytes size))]
-        (println f (fmt-bytes size) (mapv type [out msg nonce key]))
-        (bench (f out msg nonce key))))
+  (println "secretbox to-buf! with direct bufs, pre-allocation")
+  (bench-secretnonce [s/secretbox-easy-to-direct-byte-bufs-with-macros!
+                      s/secretbox-easy-to-direct-byte-bufs!
+                      s/secretbox-easy-to-byte-bufs-nocast!
+                      s/secretbox-easy-refl!]
+                     bc/->direct-byte-buf)
 
-    (println "secretbox to-buf! with indirect bufs")
-    (println "these bufs already exist, so there is no allocation")
-    (doseq [[size msg] (map (juxt identity random-indirect-byte-buf) sizes)
-            f [s/secretbox-easy-to-indirect-byte-bufs-with-macros!
-               s/secretbox-easy-to-indirect-byte-bufs!
-               s/secretbox-easy-to-byte-bufs-nocast!
-               s/secretbox-easy-refl!]]
-      (let [key (random-indirect-byte-buf s/keybytes)
-            nonce (random-indirect-byte-buf s/noncebytes)
-            out (ByteBuffer/allocate (+ s/macbytes size))]
-        (println f (fmt-bytes size) (mapv type [out msg nonce key]))
-        (bench (f out msg nonce key))))
+  (println "secretbox to-buf! with indirect bufs, pre-allocation")
+  (bench-secretnonce [s/secretbox-easy-to-indirect-byte-bufs-with-macros!
+                      s/secretbox-easy-to-indirect-byte-bufs!
+                      s/secretbox-easy-to-byte-bufs-nocast!
+                      s/secretbox-easy-refl!]
+                     bc/->indirect-byte-buf)
 
-    (println "secretbox to-buf! with byte arrays")
-    (println "these bufs already exist, so there is no allocation")
-    (doseq [[size msg] (map (juxt identity randombytes) sizes)
-            f [s/secretbox-easy-to-buf!]]
-      (let [key (randombytes s/keybytes)
-            nonce (randombytes s/noncebytes)
-            out (byte-array (+ s/macbytes size))]
-        (println f (fmt-bytes size) (mapv type [out msg nonce key]))
-        (bench (f out msg nonce key))))))
+  (println "secretbox to-buf! with byte arrays, pre-allocation")
+  (bench-secretnonce [s/secretbox-easy-to-buf!] identity))
