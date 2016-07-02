95fd4b308c23e1f45281702eeabc0bc29ca6e7ca
lvh <_@lvh.io>
2016-07-02 11:25:18 -0500

    Document byte bufs fns

--- src/caesium/byte_bufs.clj
+++ src/caesium/byte_bufs.clj
@@ -4,10 +4,25 @@
   (:import (java.nio ByteBuffer)))
 
 (defn ^ByteBuffer ->indirect-byte-buf
+  "Convert buffer to a [[ByteBuffer]].
+
+  If the input is not a byte buffer already, will be converted to an
+  indirect byte buffer. For example, a byte array will just be
+  wrapped. If the input is already a byte buffer, return it
+  unmodified, *even if it was a direct byte buffer*. (This might seem
+  confusing, but is generally what callers of this API want.)"
   [x]
   (bs/convert x ByteBuffer {:direct? false}))
 
 (defn ^ByteBuffer ->direct-byte-buf
+  "Convert buffer to a [[ByteBuffer]].
+
+  If the input is not a byte buffer already, will be converted to a
+  direct byte buffer. For example, a byte array will be copied into a
+  newly allocated direct byte buffer. If the input is already a byte
+  buffer, return it unmodified, *even if it was an indirect byte
+  buffer*. (This might seem confusing, but is generally what callers
+  of this API want.)"
   [x]
   (bs/convert x ByteBuffer {:direct? true}))
 
