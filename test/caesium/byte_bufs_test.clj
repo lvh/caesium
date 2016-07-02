046f1a25e9a12dc1eb9cad6040ac38ac208db278
lvh <_@lvh.io>
2016-07-02 11:25:30 -0500

    Add tests for byte buf conversion fns

--- test/caesium/byte_bufs_test.clj
+++ test/caesium/byte_bufs_test.clj
@@ -3,6 +3,7 @@
             [clojure.test :refer [deftest is]]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
+            [com.gfredericks.test.chuck.clojure-test :refer [for-all]]
             [com.gfredericks.test.chuck.properties :as prop'])
   (:import (java.nio ByteBuffer)))
 
@@ -11,6 +12,32 @@
    (fn [n] (ByteBuffer/allocate n))
    (fn [n] (ByteBuffer/allocateDirect n))
    (fn [n] (ByteBuffer/wrap (byte-array n)))])
+
+(defspec ->indirect-byte-buf-spec
+  1000
+  (for-all
+   [n gen/pos-int
+    g (gen/elements ways-of-getting-a-buf-of-len-n)
+    :let [src (g n)
+          buf (bb/->indirect-byte-buf src)]]
+   (is (= n (bb/buflen src) (bb/buflen buf)))
+   (is (instance? ByteBuffer buf))
+   (when-not (instance? ByteBuffer src)
+     ;; when the input is a byte buffer, don't convert
+     (is (not (.isDirect buf))))))
+
+(defspec ->direct-byte-buf-spec
+  1000
+  (for-all
+   [n gen/pos-int
+    g (gen/elements ways-of-getting-a-buf-of-len-n)
+    :let [src (g n)
+          buf (bb/->direct-byte-buf src)]]
+   (is (= n (bb/buflen src) (bb/buflen buf)))
+   (is (instance? ByteBuffer buf))
+   (when-not (instance? ByteBuffer src)
+     ;; when the input is a byte buffer, don't convert
+     (is (.isDirect buf)))))
 
 (defspec buflen-spec
   1000
