2f6f85ba044f02573b2514997db9911d14256003
lvh <_@lvh.io>
2016-07-02 09:57:29 -0500

    Use new buflen protocol

--- src/caesium/crypto/sign.clj
+++ src/caesium/crypto/sign.clj
@@ -1,6 +1,7 @@
 (ns caesium.crypto.sign
   (:refer-clojure :exclude [bytes])
-  (:require [caesium.binding :refer [defconsts sodium]]))
+  (:require [caesium.binding :refer [defconsts sodium]]
+            [caesium.byte-bufs :refer [buflen]]))
 
 (defconsts [bytes seedbytes publickeybytes secretkeybytes primitive])
 
@@ -27,23 +28,21 @@
   "Puts a signed version of the given message using given secret key into the
   given out buffer."
   [out sk m]
-  (let [mlen (alength ^bytes m)]
-    (.crypto_sign sodium out nil m mlen sk)
-    out))
+  (.crypto_sign sodium out nil m (buflen m) sk)
+  out)
 
 (defn signed
   "Produces a signed version of the given message m using given secret key."
   [sk m]
-  (let [sm (byte-array (+ bytes (alength ^bytes m)))]
+  (let [sm (byte-array (+ bytes (buflen m)))]
     (signed-to-buf! sm sk m)))
 
 (defn sign-to-buf!
   "Puts a signature of the given message using given secret key into the given
   out buffer."
   [out sk m]
-  (let [mlen (alength ^bytes m)]
-    (.crypto_sign_detached sodium out nil m mlen sk)
-    out))
+  (.crypto_sign_detached sodium out nil m (buflen m) sk)
+  out)
 
 (defn sign
   "Produces a detached signature for a message m using given secret key."
@@ -58,14 +57,14 @@
   message. When given a valid signature, returns nil. When given an
   invalid signed message or signature, raises RuntimeException."
   ([pk sm]
-   (let [smlen (alength ^bytes sm)
+   (let [smlen (buflen sm)
          m (byte-array (- smlen bytes))
          res (.crypto_sign_open sodium m nil sm smlen pk)]
      (if (zero? res)
        m
        (throw (RuntimeException. "Signature validation failed")))))
   ([pk msg sig]
-   (let [mlen (alength ^bytes msg)
+   (let [mlen (buflen msg)
          res (.crypto_sign_verify_detached sodium sig msg mlen pk)]
      (when-not (zero? res)
        (throw (RuntimeException. "Signature validation failed"))))))
