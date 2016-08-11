0df89ef9fadc9fc5195392f4efabd6474a231b30
lvh <_@lvh.io>
2016-08-11 16:42:35 +0900

    Move secretbox to new API

--- src/caesium/crypto/secretbox.clj
+++ src/caesium/crypto/secretbox.clj
@@ -2,7 +2,7 @@
   "Bindings to the secretbox secret-key authenticated encryption scheme."
   (:require [caesium.binding :as b]
             [caesium.util :as u]
-            [caesium.byte-bufs :refer [buflen]])
+            [caesium.byte-bufs :as bb])
   (:import [java.nio ByteBuffer]))
 
 (b/defconsts [keybytes noncebytes macbytes primitive])
@@ -15,7 +15,7 @@
   You only want this to manage the output byte buffer yourself. Otherwise,
   you want [[secretbox-easy]]."
   [c m n k]
-  (b/✨ easy m n k))
+  (b/✨ easy c m n k))
 
 (defn secretbox-easy
   "Encrypt with `crypto_secretbox_easy`.
@@ -28,41 +28,26 @@
   automatically prepend the required `ZERO_BYTES` NUL bytes, verify
   that the encryption succeeded, and strip them from the returned
   ciphertext."
-  [msg nonce key]
-  (let [out (byte-array (+ macbytes (buflen msg)))]
-    (secretbox-easy-to-buf! out msg nonce key)
-    out))
+  [m n k]
+  (let [c (bb/alloc (+ macbytes (bb/buflen m)))]
+    (secretbox-easy-to-buf!
+     c
+     (bb/->indirect-byte-buf m)
+     (bb/->indirect-byte-buf n)
+     (bb/->indirect-byte-buf k))
+    (bb/->bytes c)))
 
 (defn secretbox-open-easy-to-buf!
   "Decrypt with `crypto_secretbox_open_easy` into the given byte array.
 
+  All arguments must be `java.nio.ByteBuffer`.
+
   You only want this to manage the output byte array yourself. Otherwise,
   you want [[secretbox-open-easy]]."
-  [^bytes out ^bytes ctext ^bytes nonce ^bytes key]
-  (let [clen (long (buflen ctext))
-        res (.crypto_secretbox_open_easy b/sodium out ctext clen nonce key)]
+  [m c n k]
+  (let [res (b/✨ open-easy m c n k)]
     (if (= res 0)
-      out
-      (throw (RuntimeException. "Ciphertext verification failed")))))
-
-(defn secretbox-open-easy-from-byte-bufs!
-  "**WARNING** low-level API, specialized use!
-
-  This function is probably only useful if you're using ByteBuffers to manage
-  the layout of a byte array that contains both the nonce and the
-  secretbox-easy ciphertext. If you're not sure, this is not the API you want;
-  check out [[secretbox-open-easy]] instead."
-  [out ctext ctextlen nonce key]
-  ;; This takes an ctextlen argument because you can't tell the appropriate
-  ;; length from the ctext ByteBuffer and the caller knows anyway.
-  (let [res (.crypto_secretbox_open_easy b/sodium
-                                         ^bytes out
-                                         ^java.nio.ByteBuffer ctext
-                                         ^long ctextlen
-                                         ^java.nio.ByteBuffer nonce
-                                         ^bytes key)]
-    (if (= res 0)
-      out
+      m
       (throw (RuntimeException. "Ciphertext verification failed")))))
 
 (defn secretbox-open-easy
@@ -76,23 +61,28 @@
   automatically prepend the required `BOXZERO_BYTES` NUL bytes, verify
   that the decryption succeeded, and strip them from the returned
   plaintext."
-  [ctext nonce key]
-  (let [out (byte-array (- (buflen ctext) macbytes))]
-    (secretbox-open-easy-to-buf! out ctext nonce key)))
+  [c n k]
+  (let [m (bb/alloc (- (bb/buflen c) macbytes))]
+    (secretbox-open-easy-to-buf!
+     m
+     (bb/->indirect-byte-buf c)
+     (bb/->indirect-byte-buf n)
+     (bb/->indirect-byte-buf k))
+    (bb/->bytes m)))
 
 (defn encrypt
   "Backwards-compatible alias for [[secretbox-easy]].
 
   Please note that this uses a different argument order."
-  [key nonce msg]
-  (secretbox-easy msg nonce key))
+  [k n m]
+  (secretbox-easy m n k))
 
 (defn decrypt
   "Backwards-compatible alias for [[secretbox-open-easy]].
 
   Please note that this uses a different argument order."
-  [key nonce ciphertext]
-  (secretbox-open-easy ciphertext nonce key))
+  [k n c]
+  (secretbox-open-easy c n k))
 
 (def int->nonce
   "Turns an integer into a byte array, suitable as a nonce.
