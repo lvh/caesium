57c0585d80c9037104d2a35adbfe47fa8df21b31
lvh <_@lvh.io>
2016-08-13 20:32:55 +0900

    Make the tests pass

--- src/caesium/randombytes.clj
+++ src/caesium/randombytes.clj
@@ -5,21 +5,7 @@
             [caesium.byte-bufs :as bb])
   (:import (java.nio ByteBuffer)))
 
-(defn random-to-byte-array!
-  "**DANGER** This function is low-level, you only want to use it if you are
-  managing your own buffers. See [[randombytes]] for a high level API that
-  creates the byte array buffer for you.
-
-  Given a byte array, populate it with n random bytes.
-
-  If n is not given, populates the entire array."
-  ([^bytes arr]
-   (random-to-byte-array! arr (bb/buflen arr)))
-  ([^bytes arr n]
-   (.randombytes b/sodium arr (long n))
-   arr))
-
-(defn random-to-byte-buffer!
+(defn random-to-buf!
   "**DANGER** This function is low-level, you only want to use it if you are
   managing your own buffers. See [[randombytes]] for a high level API that
   creates the buffer for you.
@@ -28,7 +14,7 @@
 
   If n is not given, populates the entire buffer."
   ([^ByteBuffer buf]
-   (random-to-byte-buffer! buf (.remaining buf)))
+   (random-to-buf! buf (.remaining buf)))
   ([^ByteBuffer buf n]
    (.randombytes b/sodium buf (long n))
    buf))
@@ -36,5 +22,4 @@
 (defn randombytes
   "Create a byte array with n random bytes."
   [n]
-  (let [buf (byte-array n)]
-    (random-to-byte-array! buf n)))
+  (bb/->bytes (random-to-buf! (bb/alloc n) n)))
