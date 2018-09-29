35aac803920c3ec37ed08ec9c3ae6df4ca000d85
Arte Ebrahimi <arteebrahimi@gmail.com>
2018-09-28 20:52:19 -0700

    more detailed hex-resources docstring

--- test/caesium/vectors.clj
+++ test/caesium/vectors.clj
@@ -9,6 +9,6 @@
   (comp u/unhexify s/trim slurp io/resource))
 
 (def hex-resources
-  "Gets a named resource that contains multiple test vectors as a 
-  collection of byte arrays."
+  "Gets a named resource  in hex format that contains multiple test
+  vectors separated by a newline as a collection of byte arrays."
   (comp #(map (comp u/unhexify s/trim) %) #(clojure.string/split % #"\n") slurp io/resource))
