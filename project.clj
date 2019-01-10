146866b46d8fdda93831397fd5d9140d7cf2c7a0
Laurens Van Houtven (lvh) <lvh@latacora.com>
2019-01-10 15:54:41 -0600

    Version 0.13.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject caesium "0.12.0"
+(defproject caesium "0.13.0-SNAPSHOT"
   :description "libsodium for clojure"
   :url "https://github.com/lvh/caesium"
   :deploy-repositories [["releases" :clojars]
