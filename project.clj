4cff7bc41ec6c6873b8d6540586cda714464bad2
Dominic Pearson <dsp@technoanimal.net>
2020-02-20 16:45:29 +0100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -10,20 +10,20 @@
                  [commons-codec/commons-codec "1.11"]
                  [byte-streams "0.2.4"]
                  [org.clojure/math.combinatorics "0.1.4"]
-                 [medley "1.0.0"]
+                 [medley "1.2.0"]
                  [com.taoensso/timbre "4.10.0"]]
   :main ^:skip-aot caesium.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[criterium "0.4.4"]
+             :dev {:dependencies [[criterium "0.4.5"]
                                   [org.clojure/test.check "0.9.0"]
-                                  [com.gfredericks/test.chuck "0.2.9"]]}
+                                  [com.gfredericks/test.chuck "0.2.10"]]}
              :test {:plugins [[lein-ancient "0.6.15"]
-                              [lein-cljfmt "0.5.7"]
-                              [lein-kibit "0.1.6"]
-                              [jonase/eastwood "0.2.6"]
-                              [lein-codox "0.10.3"]
-                              [lein-cloverage "1.0.10"]]}
+                              [lein-cljfmt "0.6.6"]
+                              [lein-kibit "0.1.8"]
+                              [jonase/eastwood "0.3.7"]
+                              [lein-codox "0.10.7"]
+                              [lein-cloverage "1.1.2"]]}
              :benchmarks {:source-paths ["test/"]
                           :test-paths ^:replace ["benchmarks/"]}}
   :codox {:metadata {:doc/format :markdown}
